97377e89c33557b96364de169f9c956a8fe21c5a
CasablancaC4Q <32021306+CasablancaC4Q@users.noreply.github.com>
2017-12-19 18:40:55 -0500

    added part 01

---
+++ app/src/main/java/com/example/c4q/johnmid/LoginActivity.java
@@ -0,0 +1,10 @@
+package com.example.c4q.johnmid;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by c4q on 12/19/17.
+ */
+
+public class LoginActivity extends AppCompatActivity {
+}
